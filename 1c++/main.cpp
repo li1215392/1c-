ee2cf3969b686d609a9f120bc9b985ea109d1238
li1215392 <Projects>
2014-08-17 15:20:05 +0800

    myTest
    an objec-c++ test projiect

---
+++ 1c++/main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  1c++
+//
+//  Created by Gavin on 14-7-27.
+//  Copyright (c) 2014年 Tirank. All rights reserved.
+//
+
+#include <iostream>
+using std::endl;
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    
+    std::cout << endl
+        << "hello"
+        << endl;
+    return 0;
+}
+
